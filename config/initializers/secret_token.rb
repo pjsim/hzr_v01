b85911755674739837c86d5b03e71881186a9253
Phillip Simmonds <phillipsimmonds@ubuntu.(none)>
2013-09-22 21:31:58 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HazaarV1::Application.config.secret_key_base = 'a1eefcdf216dde1fa3cdac392c80f1ecdc1b6ad6e285e94284a8457b8f98c5689c358e620d691b463f2f44af975355116da017e1840414dbc339b94808b2747d'
